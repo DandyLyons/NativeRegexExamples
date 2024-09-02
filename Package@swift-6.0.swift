0e6377a2422cfd3a66ce9ffdd67412ec79425bdb
Daniel Lyons <72824209+DandyLyons@users.noreply.github.com>
2024-09-02 12:20:39 -0600

    Expand platform availability
    NativeRegexExamples has been expanded to support every platform that supports the `Regex` type.

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "NativeRegexExamples",
-  platforms: [.iOS(.v16), .macOS(.v13)],
+  platforms: [.iOS(.v16), .macOS(.v13), .macCatalyst(.v16), .tvOS(.v16), .visionOS(.v1), .watchOS(.v9)],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
